75d1733d0f2c3a14fb0b4dca5829ded0758d5b5a
Ilailson Rocha <ilailson@hotmail.com>
2019-06-04 19:19:20 -0300

    Correção erro data anterior. Deixando o sistema mas rápido com f:ajax

--- src/main/java/com/ferias/bean/FeriaBean.java
+++ src/main/java/com/ferias/bean/FeriaBean.java
@@ -1,13 +1,12 @@
+
 package com.ferias.bean;
 
-import java.awt.Desktop.Action;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
 
